860dae2e9d1b3e45b50c4bcfb380cab6a2e00c5b
GaelGodoi <gaelgodoi@gmail.com>
2026-05-20 14:05:16 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='meu-treino-v20-netlify';
+const CACHE_NAME='meu-treino-v23-netlify';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./grip-pronada.png','./grip-supinada.png','./grip-neutra.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
